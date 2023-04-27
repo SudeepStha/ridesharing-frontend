c3b96a55adc75df926ec2e11ed64b3f1abfce680
Tej Bahadur Shrestha <venture.tej@gmail.com>
2023-04-27 13:24:15 +0545

    initial push

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import LoginView from './views/LoginView.vue';
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 text-center antialiased">
+    <LoginView />
+    <!-- <RouterView /> -->
+  </div>
+</template>
+
---
+++ src/views/LoginView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="pt-16">
+        <h1>Enter Your Phone number.</h1>
+        <form action="#" @submit.prevent="handleLogin">
+            <div class="my-4">
+                <input v-maska data-maska="(+###) ##########" v-model="data.phone" type="text"  placeholder="(+977)  1234567890">
+            </div>
+            <div>
+                <button type="submit" @submit.prevent="handleLogin" class="bg-black text-white btn">Submit</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup>
+    import { vMaska } from "maska";
+    import { reactive } from "vue";  
+  
+    const data = reactive({
+        phone: null
+
+    });
+
+    const handleLogin = () => {
+        console.log(data.phone);
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
